32fcfbc2a6b0fcb2cc07e06760cbe1044e922c58
Zhanna Espe <35231789+Jannet-es@users.noreply.github.com>
2018-01-21 00:13:45 +0700

    Delete Задание_1.py

--- Лабораторные/Задание_1.py
+++
@@ -1,8 +0,0 @@
-import requests, re 
-r = requests.get('https://moodle.tsu.ru/')
-result = re.findall('[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}', r.text) 
-seen = []
-for i in result:
-    if i not in seen:
-        seen.append(i)
-print (seen)
